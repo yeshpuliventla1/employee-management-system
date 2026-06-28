1ea291b17c41e93b7655a99809fce4124e1416a4
EC2 Default User <ec2-user@ip-172-31-31-101.ap-southeast-2.compute.internal>
2026-06-28 19:29:22 +0000

    Updated Employee Management System banner

--- src/main/java/com/abctech/ems/EmployeeManagementApp.java
+++ src/main/java/com/abctech/ems/EmployeeManagementApp.java
@@ -3,7 +3,12 @@ package com.abctech.ems;
 public class EmployeeManagementApp {
 
     public static void main(String[] args) {
-
+	
+	System.out.println("=================================");
+        System.out.println("Employee Management System v2.0");
+        System.out.println("Built by Jenkins CI Pipeline");
+        System.out.println("=================================");
+	
         EmployeeService service = new EmployeeService();
 
         service.addEmployee(new Employee(101, "Rahul", "IT"));
